637fe00136db1ea1bdf120c4fccfb75dafa40731
Oleksii <bgeota@gmail.com>
2018-11-27 17:05:43 +0200

    create DetailBrand

--- src/main/java/vo/DetailBrand.java
+++ src/main/java/vo/DetailBrand.java
@@ -1,4 +1,18 @@
 package vo;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Table
 public class DetailBrand {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "brandId")
+    private Brand brand;
+    @Column(name = "detailNumberId")
+    private DetailNumber detailNumber;
+
 }
