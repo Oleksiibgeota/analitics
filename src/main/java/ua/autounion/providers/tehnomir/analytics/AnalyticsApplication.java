8a0480c7358ebaf0c0e9507454e6ccd60ba8089c
Oleksii <bgeota@gmail.com>
2018-11-27 16:15:36 +0200

    first commit

---
+++ src/main/java/ua/autounion/providers/tehnomir/analytics/AnalyticsApplication.java
@@ -0,0 +1,12 @@
+package ua.autounion.providers.tehnomir.analytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyticsApplication.class, args);
+	}
+}
