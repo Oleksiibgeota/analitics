e0acd03434e213e56f4ae3b4846f0b970b9aa7ed
Oleksii <bgeota@gmail.com>
2018-11-27 17:21:56 +0200

    change class DeliveryType

--- src/main/java/vo/DeliveryType.java
+++ src/main/java/vo/DeliveryType.java
@@ -2,8 +2,15 @@ package vo;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
+@Table
 public class DeliveryType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column
     private String type;
 }
