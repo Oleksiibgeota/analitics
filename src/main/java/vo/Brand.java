d17661d60103be82b69ee7f8f8a16887226d9830
Oleksii <bgeota@gmail.com>
2018-11-27 17:18:11 +0200

    change class Brand

--- src/main/java/vo/Brand.java
+++ src/main/java/vo/Brand.java
@@ -2,9 +2,17 @@ package vo;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Table
+@Entity
 public class Brand {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column
     private String name;
+    @Column
     private boolean original;
 }
